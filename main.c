1ed79230c90983afd999acde7b658ffc28266a4f
crazywire <35847973+crazywire@users.noreply.github.com>
2018-01-26 14:19:44 -0400

    Update main.c

--- main.c
+++ main.c
@@ -27,86 +27,18 @@ ISR(INT0_vect){
 	button_status = (PIND & (1<<PD2));
 	
 	
-	//Apply original signal to PB0
-	if (button_status)
-	{
-		PORTB |= (1<<PB0); 
-	}else{
-		PORTB &= ~(1<<PB0);
-	}
 	
-	//If the switch is opened, transition from high to low
-	if (my_current_state == high)
-	{
-		if (!(button_status))
-		{
-			my_current_state = trans_hl;
-			sample_counts = 0;
-			good_sample_counts = 0;
-	
-		}
-	}
-	
-	//If the switch is closed, transition from low to high 
-	if (my_current_state == low)
-	{
-		if (button_status)
-		{
-			my_current_state = trans_lh;
-			sample_counts = 0;
-			good_sample_counts = 0;
-		}
-	}
 }
 
 ISR(TIMER0_COMPA_vect){
 	
-	if (sample_counts<= total_sample_points)
-	{
-		sample_counts++;
-		}else{
-			sample_counts = 0;
-			good_sample_counts = 0;
-	}
 	
-	//if in transition count the number of the same successive lows or highs
-	if (my_current_state == trans_lh)
-	{
-		if (button_status)
-		{
-			good_sample_counts++;
-		}
-	}
-	
-	if (my_current_state == trans_hl)
-	{
-		if (!(button_status))
-		{
-			good_sample_counts++;
-		}
-	}
-
 }
 
 
 void debounce_button(){
 	
-	if (my_current_state == trans_lh)
-	{
-		if (good_sample_counts >= min_sample_points)
-		{
-			confirmed_button_status = 1;
-			my_current_state = high;
-		}
-	}
-	if (my_current_state == trans_hl)
-	{
-		if (good_sample_counts >= min_sample_points)
-		{
-			confirmed_button_status = 0;
-			my_current_state = low;
-		}
-	}
+	
 }
 
 int main(void)
@@ -115,16 +47,6 @@ int main(void)
 	DDRD &= ~(1<<PD2);
 	
 	
-	//Decide if the switch is open or closed
-
-	if (PIND & (1<<PD2))
-	{
-		my_current_state = high;
-		PORTB |= (1<<PB0) | (1<<PB1);
-	}else{
-		my_current_state = low;
-		PORTB &= ~ (1<<PB0) & ~(1<<PB1);
-	}
 	
 	EIMSK = (1<<INT0);
 	EICRA |= (1<<ISC00);
